8b903e8098a1a950ec984cbe8fe31af8974bc5e9
raitonoberu <raitonoberu@mail.ru>
2023-02-06 22:38:06 +0500

    Don't compile mpris on Windows & Darwin

---
+++ services/mpris/mpris_windows.go
@@ -0,0 +1,17 @@
+package mpris
+
+import (
+	"errors"
+	"sptlrx/player"
+)
+
+func New(name string) (*Client, error) {
+	return nil, errors.New("windows is not supported")
+}
+
+// Client implements player.Player
+type Client struct{}
+
+func (p *Client) State() (*player.State, error) {
+	return nil, nil
+}
