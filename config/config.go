6cb834eb1a5ca9500a1d3ed515bf273ada31381e
raitonoberu <raitonoberu@mail.ru>
2023-05-13 16:06:29 +0500

    Improve formatting

--- config/config.go
+++ config/config.go
@@ -194,9 +194,7 @@ func GetPlayer(conf *Config) (player.Player, error) {
 	case "mpd":
 		return mpd.New(conf.Mpd.Address, conf.Mpd.Password)
 	case "mopidy":
-		return mopidy.New(
-			conf.Mopidy.Address,
-		)
+		return mopidy.New(conf.Mopidy.Address)
 	case "mpris":
 		return mpris.New(conf.Mpris.Players)
 	}
